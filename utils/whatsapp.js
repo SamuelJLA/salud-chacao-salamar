09e98864dff058dc2f64487956158158cf615518
SamuelJLA <samuellopezalcala198@gmail.com>
2026-08-19 21:35:43 -0400

    Delete utils directory

--- utils/whatsapp.js
+++
@@ -1,8 +0,0 @@
-export const createWhatsAppLink = (serviceName, providerLabel, phone) => {
-  const cleanPhone = phone ? phone.replace(/\D/g, '') : '584141742148';
-  const text = encodeURIComponent(
-    `Hola, quisiera cotizar y consultar disponibilidad para el siguiente servicio:\n\n📌 *${serviceName}*\n🏢 *${providerLabel}*\n\n¿Me podrían brindar información sobre costos y requisitos? ¡Muchas gracias!`
-  );
-
-  return `https://wa.me/${cleanPhone}?text=${text}`;
-};
